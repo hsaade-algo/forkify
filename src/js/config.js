e588cee62f56ae33112a5163547606bc9477477b
hsaade-algo <hsaade@hotmail.com>
2020-12-22 08:35:54 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dca0fdf1-e5ce-4875-bb99-ec9e27b97953';
+export const MODAL_CLOSE_SEC = 2.5;
